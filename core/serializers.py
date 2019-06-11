c71575274213fe2a5addcbf81c2fd932069c0c0d
Gustavo Soares <gustavo@papaga.io>
2019-06-11 17:05:29 -0300

    teacher + student create ok

--- core/serializers.py
+++ core/serializers.py
@@ -5,15 +5,54 @@ from . import models
 
 class TeacherSerializer(serializers.ModelSerializer):
 
+    pk = serializers.UUIDField(read_only=True, source='idTeacher')
+    password = serializers.CharField(write_only=True)
+    username = serializers.CharField(write_only=True)
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username já cadastrado.")
+        return value
+
+    def create(self, validated_data):
+        username = validated_data.pop('username')
+
+        user = User.objects.create(username=username, first_name=validated_data['name'], email=validated_data['email'])
+        user.set_password(validated_data.pop('password'))
+        user.save()
+
+        teacher = models.Teacher.objects.create(**validated_data, user=user)
+        return teacher
+
     class Meta:
         model = models.Teacher
-        fields = ('idTeacher','name', 'email', 'dateOfBirth', 'gender', 'user')
+        fields = ('pk', 'name', 'email', 'dateOfBirth', 'gender', 'username', 'password')
         
 
 class StudentSerializer(serializers.ModelSerializer):
+
+    pk = serializers.UUIDField(read_only=True, source='idStudent')
+    password = serializers.CharField(write_only=True)
+    username = serializers.CharField(write_only=True)
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username já cadastrado.")
+        return value
+
+    def create(self, validated_data):
+        username = validated_data.pop('username')
+
+        user = User.objects.create(username=username, first_name=validated_data['name'], email=validated_data['email'])
+        user.set_password(validated_data.pop('password'))
+        user.save()
+
+        teacher = models.Student.objects.create(**validated_data, user=user)
+        return teacher
+
     class Meta:
         model = models.Student
-        fields = ('idStudent','name', 'email', 'dateOfBirth', 'course', 'gender', 'user')
+        fields = ('pk', 'name', 'email', 'dateOfBirth', 'course', 'gender', 'username', 'password')
 
 
 
