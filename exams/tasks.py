a150e2c586edd864995a499dda0df3c10c3c012b
fernandasj <fernanda.silva_sj@hotmail.com>
2020-03-13 13:02:36 -0300

    formatando arquivo para correção

--- exams/tasks.py
+++ exams/tasks.py
@@ -9,15 +9,21 @@ from Docker.main import build_image
 
 @task()
 def correctionCode(pk):
-	# head = str.encode("aW1wb3J0IHN5cwpmcm9tIHVuaXR0ZXN0Lm1vY2sgaW1wb3J0IE1hZ2ljTW9jawoKbHN0ID0gc3lzLmFyZ3ZbMTpdCmlucHV0ID0gTWFnaWNNb2NrKHNpZGVfZWZmZWN0PWxzdCk=")
+	head = ''' 
+import sys
+from unittest.mock import MagicMock
+
+lst = sys.argv[1:]
+input = MagicMock(side_effect=lst)
+'''
+
 	answer = exams.Answer.objects.get(pk=pk)
 	tests_case = exams.CodeAnswer.objects.filter(question_id=answer.question_id)
 
 	decode = base64.b64decode(answer.textAnswer)
-	# headFile = base64.b64decode(str(head))
 
 	code = open('Docker/' + str(answer.pk) + '.py', 'a')
-	# code.write(decode.headFile('utf-8'))
+	code.write(head)
 	code.write(decode.decode('utf-8'))
 	code.close()
 	
